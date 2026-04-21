7ee9741528bd61fddb5b1fc64998098510ce7f6b
JentleJoe <aigbokhaebholojoshua@gmail.com>
2026-04-21 18:14:25 +0100

    unistalled react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 
-const queryClient = new QueryClient();
-
 const App = () => (
   <div className="m-0 p-0">
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </div>
 );
 
--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,18 +1,13 @@
 import { renderToString } from "react-dom/server";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StaticRouter } from "react-router-dom/server";
 import AppRoutes from "./AppRoutes";
 
 export const render = (url) => {
-  const queryClient = new QueryClient();
-
   const appHtml = renderToString(
     <div className="m-0 p-0">
-      <QueryClientProvider client={queryClient}>
-        <StaticRouter location={url}>
-          <AppRoutes />
-        </StaticRouter>
-      </QueryClientProvider>
+      <StaticRouter location={url}>
+        <AppRoutes />
+      </StaticRouter>
     </div>,
   );
 
