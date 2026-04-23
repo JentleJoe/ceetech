a89d7f4f883c36b761b44f9927be0ad504c8f5fa
JentleJoe <aigbokhaebholojoshua@gmail.com>
2026-04-23 14:20:34 +0100

    fixed console errors related to manifest icon and react-router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppRoutes from "./AppRoutes";
 
 const App = () => (
   <div className="m-0 p-0">
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AppRoutes />
     </BrowserRouter>
   </div>
